8bcdf2ec478538280738e55ea7887ae31d41d167
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-17 16:49:11 +0200

    HTLM and CSS added and the class called

--- scripts.js
+++ scripts.js
@@ -19,7 +19,90 @@ class bookPreview extends HTMLElement {
     }
   }
 
-  
+  render() {
+    const author = this.getAttribute("author");
+    const image = this.getAttribute("image");
+    const id = this.getAttribute("id");
+    const title = this.getAttribute("title");
+
+    const template = document.createElement("template");
+    template.innerHTML = `
+      <style>
+        .preview {
+          border-width: 0;
+          width: 100%;
+          font-family: Roboto, sans-serif;
+          padding: 0.5rem 1rem;
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+          text-align: left;
+          border-radius: 8px;
+          border: 1px solid rgba(var(--color-dark), 0.15);
+          background: rgba(var(--color-light), 1);
+        }
+
+        @media (min-width: 60rem) {
+          .preview {
+            padding: 1rem;
+          }
+        }
+
+        .preview_hidden {
+          display: none;
+        }
+
+        .preview:hover {
+          background: rgba(var(--color-blue), 0.05);
+        }
+
+        .preview__image {
+          width: 48px;
+          height: 70px;
+          object-fit: cover;
+          background: grey;
+          border-radius: 2px;
+          box-shadow: 
+            0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+            0px 1px 1px 0px rgba(0, 0, 0, 0.1), 
+            0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+        }
+
+        .preview__info {
+          padding: 1rem;
+        }
+
+        .preview__title {
+          margin: 0 0 0.5rem;
+          font-weight: bold;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;  
+          overflow: hidden;
+          color: rgba(var(--color-dark), 0.8)
+        }
+
+        .preview__author {
+          color: rgba(var(--color-dark), 0.4);
+        }
+      </style>
+
+      <button class="preview" data-preview="${id}">
+        <img
+            class="preview__image"
+            src="${image}"
+        />
+
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${authors[author]}</div>
+        </div>
+      </button>
+    `;
+    this.shadowRoot.innerHTML = "";
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
 }
 
 
@@ -40,6 +123,7 @@ function renderBooks(books) {
   document.querySelector("[data-list-items]").appendChild(fragment);
   showMore(); //Calls this function to update the "Show more" button.
 }
+customElements.define("book-preview", bookPreview);
 
 //Creates element to display the books on screen as a button with and image, title and author.
 function createBookElement({ author, id, image, title }) {
